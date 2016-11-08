8d9b0239b4cca0e5d2c03eba2600536e64ecb1e2
zeneke <zeneke@gmail.com>
2016-11-08 11:38:08 +0100

    moved to godot engine

---
+++ src/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "Dog shitting"
+	t.version = "0.10.2"
+	t.window.width = 800
+	t.window.height = 480
+
+	-- For windows debuging
+	t.console = true
+end
--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,24 @@
+-- Shitting dog
+
+debug = true
+
+dog = { x = 200, y = 210, speed = 150, img = nil}
+
+function love.load()
+	x, y, w, h = 20, 20, 60, 20
+	love.graphics.setColor(100, 100, 100)
+	dog.img = love.graphics.rectangle("fill", 0, 0, 100, 100)
+end
+
+function love.update(dt)
+	w = w + 1
+	h = h + 1
+end
+
 function love.draw()
 	love.graphics.print('Hello World', 400, 300)
+	love.graphics.setColor(0, 100, 100)
+	love.graphics.rectangle("fill", x , y , w, h)
+	love.graphics.setColor(100, 0, 0)
+	love.graphics.rectangle("fill", dog.x, dog.y, 100, 100)
 end
