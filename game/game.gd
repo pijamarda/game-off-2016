709c574239fe2f1eb70bf7f53be4ee3fb5c985a2
pijamarda <zeneke@gmail.com>
2016-11-17 23:15:26 +0100

    added collision, but i need a way to reset the game

--- game/game.gd
+++ game/game.gd
@@ -7,7 +7,7 @@ extends Node2D
 # var b="textvar"
 var screen_size
 var horse_speed = 150
-var horse_direction = Vector2(1, 0)
+var horse_direction = 1
 var TOP_POSITION_Y = 64
 var MID_POSITION_Y = 288
 var BOT_POSITION_Y = 480
@@ -26,29 +26,39 @@ func _input(event):
 	if(event.is_action("horse_up") and event.is_pressed() and !event.is_echo() ):
 		print("pulso arriba")
 		if (horse_state == 0):
-			horse_pos.y = TOP_POSITION_Y
+			var temp = get_node("map/horse/kinematic_horse").get_pos().x
+			get_node("map/horse/kinematic_horse").move_to(Vector2(temp,TOP_POSITION_Y))
 			horse_state = -1
 		elif (horse_state == 1):
-			horse_pos.y = MID_POSITION_Y
+			var temp = get_node("map/horse/kinematic_horse").get_pos().x
+			get_node("map/horse/kinematic_horse").move_to(Vector2(temp,MID_POSITION_Y))
 			horse_state = 0
 	if(event.is_action("horse_down") and event.is_pressed() and !event.is_echo() ):
 		print("pulso abajo")
 		if (horse_state == -1):
-			horse_pos.y = MID_POSITION_Y
+			var temp = get_node("map/horse/kinematic_horse").get_pos().x
+			get_node("map/horse/kinematic_horse").move_to(Vector2(temp,MID_POSITION_Y))
 			horse_state = 0
 		elif (horse_state == 0):
-			horse_pos.y = BOT_POSITION_Y
+			var temp = get_node("map/horse/kinematic_horse").get_pos().x
+			get_node("map/horse/kinematic_horse").move_to(Vector2(temp,BOT_POSITION_Y))
 			horse_state = 1
+	if(event.is_action("horse_left") and event.is_pressed() and !event.is_echo() ):
+		print("pulso left")
+		horse_direction = -1
+	if(event.is_action("horse_right") and event.is_pressed() and !event.is_echo() ):
+		print("pulso right")
+		horse_direction = 1
 
 func _fixed_process(delta):
-	#horse_pos = get_node("map/horse").get_pos()
-	horse_pos += horse_direction * horse_speed * delta
-	get_node("map/horse/horse2").move(Vector2(0,1))
-	
-	if (horse_pos.x < 32 or horse_pos.x > screen_size.x - 32):
-	    horse_direction.x = -horse_direction.x
-	get_node("map/horse").set_pos(horse_pos)
-	
+	if get_node("map/horse/kinematic_horse").is_colliding():
+		print("Mueres")
+		get_node("map/horse").set_pos(Vector2(16,304))
+		#get_node("map/horse/kinematic_horse").set_pos(Vector2(16,304))
+	else:
+		get_node("map/horse/kinematic_horse").move(Vector2(horse_direction,0))
+		if (get_node("map/horse/kinematic_horse").get_pos().x < 0 or get_node("map/horse/kinematic_horse").get_pos().x > screen_size.x - 32):
+		    horse_direction = -horse_direction
 	
 func _draw():
 	
