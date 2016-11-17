6c2cf0c5325b736e96ec0bff845fbf76103af376
zeneke <zeneke@gmail.com>
2016-11-17 17:49:16 +0100

    added firewalls to maps

--- game/game.gd
+++ game/game.gd
@@ -7,24 +7,40 @@ extends Node2D
 var screen_size
 var horse_speed = 150
 var horse_direction = Vector2(1, 0)
+var TOP_POSITION_Y = 64
+var MID_POSITION_Y = 288
+var BOT_POSITION_Y = 480
+var horse_state = 0 # -1 means top, 0 mid, 1 bottom
+var horse_pos = Vector2(32,288)
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	screen_size = get_viewport_rect().size
+	set_process_input(true)
 	set_process(true)
 	
+func _input(event):
 	
+	if(event.is_action("horse_up") and event.is_pressed() and !event.is_echo() ):
+		print("pulso arriba")
+		if (horse_state == 0):
+			horse_pos.y = TOP_POSITION_Y
+			horse_state = -1
+		elif (horse_state == 1):
+			horse_pos.y = MID_POSITION_Y
+			horse_state = 0
+	if(event.is_action("horse_down") and event.is_pressed() and !event.is_echo() ):
+		print("pulso abajo")
+		if (horse_state == -1):
+			horse_pos.y = MID_POSITION_Y
+			horse_state = 0
+		elif (horse_state == 0):
+			horse_pos.y = BOT_POSITION_Y
+			horse_state = 1
+
 func _process(delta):
-	if(Input.is_action_pressed("horse_up")):
-		var cur_pos = self.get_pos()
-		cur_pos.x += 1
-		self.set_pos(cur_pos)
-	var horse_pos = get_node("map/horse").get_pos()
-	if(Input.is_action_pressed("horse_up")):
-		horse_pos.y = horse_pos.y - 50
-	if(Input.is_action_pressed("horse_down")):
-		horse_pos.y = horse_pos.y + 50
+	#horse_pos = get_node("map/horse").get_pos()
 	horse_pos += horse_direction * horse_speed * delta
 	
 	if (horse_pos.x < 32 or horse_pos.x > screen_size.x - 32):
