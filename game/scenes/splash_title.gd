66789b87abddeb99a83c22e112288cfe113f175d
pijamarda <zeneke@gmail.com>
2016-11-29 22:26:15 +0100

    added 3 seconds

--- game/scenes/splash_title.gd
+++ game/scenes/splash_title.gd
@@ -3,7 +3,7 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var time = 0.5
+var time = 3
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -12,7 +12,6 @@ func _ready():
 	pass
 
 func _process(delta):
-	time = time - delta
-	print(time)
+	time = time - delta	
 	if time < 0:
 		get_node("/root/main").run()
